cc23f903e339c25c128e06ce50e25feca97d391e
Russell <rshtyrev@gmail.com>
2023-12-26 18:15:34 +0300

    constraints corrected (tested on 15, 15Pro, SE)

--- Lesson 24 Homework/ViewController.swift
+++ Lesson 24 Homework/ViewController.swift
@@ -11,7 +11,7 @@ import WebKit
 class ViewController: UIViewController {
     
     private var webView = WKWebView()
-    var addressTextField = UITextField()
+    var addressTextField = UISearchBar()
     var forwardButton = UIButton()
     var backwardButton = UIButton()
     var reloadButton = UIButton()
@@ -33,17 +33,6 @@ class ViewController: UIViewController {
         
         guard let url = URL(string: "https://google.com") else { return }
         
-
-        self.view.addSubview(webView)
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        webView.load(URLRequest(url: url))
-        NSLayoutConstraint.activate([
-            webView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            webView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            webView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
-            webView.heightAnchor.constraint(equalToConstant: 700)
-        ])
-        
         
         
         
@@ -55,12 +44,35 @@ class ViewController: UIViewController {
         reloadButton.addTarget(self, action: #selector(buttonReloadPressed), for: .touchUpInside)
         NSLayoutConstraint.activate([
             reloadButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            reloadButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 400),
+            reloadButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             reloadButton.widthAnchor.constraint(equalToConstant: 40),
             reloadButton.heightAnchor.constraint(equalToConstant: 40)
         ])
         
         
+        self.view.addSubview(addressTextField)
+        addressTextField.translatesAutoresizingMaskIntoConstraints = false
+        addressTextField.layer.cornerRadius = 9.0
+        addressTextField.placeholder = "Web-search or URL"
+        addressTextField.text = "\(url)"
+        NSLayoutConstraint.activate([
+            addressTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            addressTextField.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            addressTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            addressTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+        ])
+        
+        
+        self.view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.load(URLRequest(url: url))
+        NSLayoutConstraint.activate([
+            webView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            webView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            webView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            webView.topAnchor.constraint(equalTo: addressTextField.bottomAnchor),
+            webView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -80)
+        ])
         
         self.view.addSubview(forwardButton)
         forwardButton.translatesAutoresizingMaskIntoConstraints = false
@@ -68,13 +80,11 @@ class ViewController: UIViewController {
         forwardButton.tintColor = .darkGray
         forwardButton.addTarget(self, action: #selector(buttonForwardPressed), for: .touchUpInside)
         NSLayoutConstraint.activate([
-            forwardButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 170),
-            forwardButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 400),
+            forwardButton.centerXAnchor.constraint(equalTo: webView.centerXAnchor, constant: 160),
+            forwardButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             forwardButton.widthAnchor.constraint(equalToConstant: 40),
             forwardButton.heightAnchor.constraint(equalToConstant: 40)
         ])
-        
-        
         
         self.view.addSubview(backwardButton)
         backwardButton.translatesAutoresizingMaskIntoConstraints = false
@@ -82,26 +92,10 @@ class ViewController: UIViewController {
         backwardButton.tintColor = .darkGray
         backwardButton.addTarget(self, action: #selector(buttonBackwardPressed), for: .touchUpInside)
         NSLayoutConstraint.activate([
-            backwardButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -170),
-            backwardButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 400),
+            backwardButton.centerXAnchor.constraint(equalTo: webView.centerXAnchor, constant: -160),
+            backwardButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             backwardButton.widthAnchor.constraint(equalToConstant: 40),
             backwardButton.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        
-        self.view.addSubview(addressTextField)
-        addressTextField.translatesAutoresizingMaskIntoConstraints = false
-        addressTextField.borderStyle = .roundedRect
-        addressTextField.layer.borderWidth = 1.0
-        addressTextField.layer.borderColor = UIColor.darkGray.cgColor
-        addressTextField.layer.cornerRadius = 9.0
-        addressTextField.placeholder = "Web-search or URL"
-        addressTextField.text = "\(url)"
-        NSLayoutConstraint.activate([
-            addressTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -30),
-            addressTextField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -380),
-            addressTextField.widthAnchor.constraint(equalToConstant: 330),
-            addressTextField.heightAnchor.constraint(equalToConstant: 50)
         ])
         
         
